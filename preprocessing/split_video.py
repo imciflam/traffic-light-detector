483d24b2770d4ab8693b207fc8d5a2a5b7b66f9c
Vita <vitalia@myrambler.ru>
2020-11-30 16:44:44 +0300

    fixed route and cycle

--- preprocessing/split_video.py
+++ preprocessing/split_video.py
@@ -1,5 +1,5 @@
 
-#python split_video.py --pathIn /Users/user/downloads/ds1/3_20191113-164801_4003p0.mp4 --pathOut /Users/user/Documents/GitHub/traffic-light-detector/preprocessing
+#python split_video.py --pathIn /Users/user/downloads/ds1/3_20191113-164801_4003p0.mp4
 import sys
 import argparse
 import os
@@ -11,17 +11,16 @@ def extractImages(pathIn, pathOut):
     vidcap = cv2.VideoCapture(pathIn)
     success,image = vidcap.read()
     success = True
-    while success:
+    while success & count <300:
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
         success,image = vidcap.read()
-        print ('Read a new frame: ', success)
-        cv2.imwrite(os.path.abspath(os.getcwd())+"\\image_folder" + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+        print('Read a new frame: ', success)
+        cv2.imwrite(os.path.abspath(os.getcwd())+"/image_folder" + "/frame%d.jpg" % count, image)    # save frame as JPEG file
         count = count + 1
 
 if __name__=="__main__":
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to video")
-    a.add_argument("--pathOut", help="path to images")
     args = a.parse_args()
     print(args)
     extractImages(args.pathIn, args.pathOut)
