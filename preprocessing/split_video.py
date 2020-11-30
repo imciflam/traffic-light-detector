ee31b4a698a6487c94db3e60812d932c64212d5e
Vita <vitalia@myrambler.ru>
2020-11-30 16:51:14 +0300

    quickfix

--- preprocessing/split_video.py
+++ preprocessing/split_video.py
@@ -6,21 +6,20 @@ import os
 import cv2
 print(cv2.__version__)
 
-def extractImages(pathIn, pathOut):
+def extractImages(pathIn):
     count = 0
     vidcap = cv2.VideoCapture(pathIn)
     success,image = vidcap.read()
     success = True
-    while success & count <300:
+    while (success and count < 300):
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
         success,image = vidcap.read()
-        print('Read a new frame: ', success)
+        print('Read a new frame: %d'% count)
         cv2.imwrite(os.path.abspath(os.getcwd())+"/image_folder" + "/frame%d.jpg" % count, image)    # save frame as JPEG file
-        count = count + 1
+        count += 1
 
 if __name__=="__main__":
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to video")
     args = a.parse_args()
-    print(args)
-    extractImages(args.pathIn, args.pathOut)
+    extractImages(args.pathIn)
