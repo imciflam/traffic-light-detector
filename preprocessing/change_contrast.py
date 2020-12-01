89546ed9953b56b0e8cd85550c4a488047af6b46
Vita <vitalia@myrambler.ru>
2020-12-01 19:15:09 +0300

    [main] + contrast tweaking for every image in directory

--- preprocessing/change_contrast.py
+++ preprocessing/change_contrast.py
@@ -6,23 +6,28 @@ import numpy as np
 
 def change_contrast(): 
     images_path = os.getcwd() + "/image_folder/"
-    #images_list = (os.listdir(images_path)) 
-    example_path = images_path+"frame-373_20191113-084800_4003p0.mp4.jpg"
-    image = cv2.imread(example_path)
-    if image is None:
-        print('Could not open or find the image')
+    images_list = (os.listdir(images_path)) 
+    for image_item in images_list:
+        count = 0
+        image = cv2.imread(images_path+image_item)
+        if image is None:
+            print('Could not open or find the image')
 
-    alpha = 1.2 # Simple contrast control
-    beta = 0.2    # Simple brightness control
-    new_image = np.zeros(image.shape, image.dtype)
+        alpha = 1.2 # Simple contrast control
+        beta = 0.2    # Simple brightness control
+        new_image = np.zeros(image.shape, image.dtype)
 
-    for y in range(image.shape[0]):
-        for x in range(image.shape[1]):
-            for c in range(image.shape[2]):
-                new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
+        for y in range(image.shape[0]):
+            for x in range(image.shape[1]):
+                for c in range(image.shape[2]):
+                    new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
+        
+        cv2.imwrite((os.getcwd())+"/image_folder_contrast/" + "/frame-"+image_item, image)
+
 
     # cv2.imshow('Original Image', image)
-    cv2.imshow('New Image', new_image)
+    # cv2.imshow('New Image', new_image)
+    
 
 
 
